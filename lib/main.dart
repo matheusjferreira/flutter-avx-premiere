d7ce80f5fb9eaa51c61e0fb27b1b97c3ebddcfe3
Matheus Ferreira <matheusferreira553@protonmail.com>
2022-08-02 09:44:51 -0300

    fix: certificate error fixed by adding file for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'app_module.dart';
 import 'app_widget.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ByteData data = await PlatformAssetBundle().load('assets/ca/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext.setTrustedCertificatesBytes(data.buffer.asUint8List());
+
   return runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
